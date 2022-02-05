ff93463ced4c7fc485745c9f1df828fcb4189a28
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-02-05 21:07:53 +0300

    feat: Use react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Order from "./pages/Order";
+import OrderList from "./components/OrderList";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello, World!</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="order" element={<Order />} />
+        <Route path="order-list" element={<OrderList />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
