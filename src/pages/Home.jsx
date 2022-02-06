2fe939bf061b91fe05341675c43adf63e1bc2986
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-02-06 22:28:44 +0300

    feat: Homepage order button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import tw, { styled } from "twin.macro";
+
+const OrderButton = styled(Link)(() => [
+  tw`bg-blue-400 capitalize rounded p-4 inline-block hover:bg-blue-500`
+]);
 
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <OrderButton to="order">order now!</OrderButton>
+    </div>
+  );
 }
 
 export default Home;
