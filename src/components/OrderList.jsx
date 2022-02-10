9337255a3e99898f8d59453018109cdb19dc620c
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-02-10 22:15:38 +0300

    feat: Display order list

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increaseQuantity,
+  decreaseQuantity,
+  removeItemFromOrderList,
+  clearOrderList
+} from "../store/orderList";
 
 function OrderList() {
-  return <div></div>;
+  const { itemsOrdered } = useSelector(state => state.orderList);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      {itemsOrdered.map(item => (
+        <div>
+          <p>{item.category}</p>
+          <p>{item.orderedQuantity}</p>
+          <button onClick={() => dispatch(increaseQuantity(item.name))}>
+            Increase
+          </button>
+          <button onClick={() => dispatch(decreaseQuantity(item.name))}>
+            decrease
+          </button>
+          <button onClick={() => dispatch(removeItemFromOrderList(item.name))}>
+            remove
+          </button>
+        </div>
+      ))}
+      <button onClick={() => dispatch(clearOrderList())}>clear</button>
+    </div>
+  );
 }
 
 export default OrderList;
