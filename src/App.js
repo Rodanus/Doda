7d573ef76341bc4b2b9f138729c491bae40306e8
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-02-21 15:45:10 +0300

    feat: Fetch food menu from firebase

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Order from "./pages/Order";
 import OrderList from "./components/OrderList";
+import { addFoodMenu } from "./store/foodMenu";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 import "./App.css";
 
+let firstRender = false;
 function App() {
   const location = useLocation();
   const state = location.state;
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (!firstRender) {
+      fetch("https://d-restaurant-38c89-default-rtdb.firebaseio.com/.json")
+        .then(res => res.json())
+        .then(res => {
+          dispatch(addFoodMenu(res.foodMenu));
+          firstRender = true;
+        });
+    }
+  });
   return (
     <div className="App">
       <Routes location={state?.backgroundLocation || location}>
