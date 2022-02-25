ad4164836563a190f248b206feb0c8361d25b74d
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-02-25 21:15:35 +0300

    feat: Homepage design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import tw, { styled } from "twin.macro";
+import tw, { styled, css } from "twin.macro";
+import HomepageBg from "../images/homepage-bg.jpg";
+
+const HomepageContainer = styled.div(() => [
+  tw`h-screen w-screen flex flex-col justify-center items-center`,
+  css`
+    background: url(${HomepageBg}) no-repeat;
+    background-position: center;
+    background-size: cover;
+  `
+]);
+
+const Overlay = styled.div(() => [
+  tw`w-screen h-screen bg-black opacity-50 absolute`
+]);
+
+const HomepageTitle = styled.h1(() => [
+  tw`text-[64px] text-white relative z-10 mb-4`
+]);
+
+const CTAContainer = styled.div(() => [tw``]);
 
 const OrderButton = styled(Link)(() => [
-  tw`bg-blue-400 capitalize rounded p-4 inline-block hover:bg-blue-500`
+  tw`relative z-10 bg-[#14131C] text-[#F3F3F3] text-[32px] capitalize rounded-full py-4 px-8 inline-block hover:bg-[#1A182D]`
 ]);
 
 function Home() {
   return (
-    <div>
-      <OrderButton to="order">order now!</OrderButton>
-    </div>
+    <HomepageContainer>
+      <Overlay />
+      <CTAContainer>
+        <HomepageTitle>D-Restaurant. Well, a Mini one...</HomepageTitle>
+        <OrderButton to="order">order now!</OrderButton>
+      </CTAContainer>
+    </HomepageContainer>
   );
 }
 
