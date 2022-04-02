168e69e2f60cd7cdd5827af88c2907d9fb2af874
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2022-04-02 14:41:51 +0300

    fix: Remove extra links

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, useLocation } from "react-router-dom";
 import Cards from "../components/Cards";
 import tw, { styled } from "twin.macro";
 
@@ -8,14 +7,9 @@ const ShopContainer = styled.main(() => [tw`container`]);
 
 function Shop() {
   const products = useSelector(store => store.products.products);
-  const location = useLocation();
   return (
     <ShopContainer>
       <Cards products={products} />
-      <Link state={{ backgroundLocation: location }} to="order-list">
-        Cart
-      </Link>
-      <Link to="/">Home</Link>
     </ShopContainer>
   );
 }
